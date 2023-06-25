f09cbcc4643200af4d7c83aec13efccee2a96189
Carlos Pedrassani <carlos.pedrassani@gruposize.com.br>
2023-06-25 09:17:07 -0300

    exercicio de jquery com validacao de formulario

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,40 @@
 $(document).ready(function () {
-  $("form").on("submit", function (e) {
-    e.preventDefault();
+  $("#telefone").mask("(00) 00000-0000");
+  $("#cpf").mask("000.000.000-00");
+  $("#cep").mask("00000-000");
 
-    const novaTarefa = $("#nova-tarefa").val();
-    const listaTarefa = $("<li></li>");
-    $(listaTarefa).text(novaTarefa);
-    $(listaTarefa).appendTo("ul");
-
-    $("#nova-tarefa").val("");
-  });
-
-  $("ul").on("click", "li", function () {
-    if ($(this).hasClass("tarefa-concluida")) {
-      $(this).removeClass("tarefa-concluida");
-    } else {
-      $(this).addClass("tarefa-concluida");
-    }
+  $("form").validate({
+    rules: {
+      nome: {
+        required: true,
+      },
+      email: {
+        required: true,
+      },
+      telefone: {
+        required: true,
+      },
+      cpf: {
+        required: true,
+      },
+      endereço: {
+        required: true,
+      },
+      cep: {
+        required: true,
+      },
+    },
+    messages: {
+      nome: "Insira o seu nome",
+      email: "Insira o seu e-mail",
+      telefone: "Insira o seu telefone",
+      cpf: "Insira o seu CPF",
+      endereço: "Insira o seu endereço",
+      cep: "Insira o seu CEP",
+    },
+    submitHandler: function (form) {
+      alert("O formulário validado com sucesso!");
+      form.submit();
+    },
   });
 });
